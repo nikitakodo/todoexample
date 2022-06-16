15a4506ec8e2b54506a5ff592b2d55000a6640c2
Nikita <m.dudin@dyvotech.io>
2022-06-16 20:37:39 +0300

    refactor error code

--- app/app/Exceptions/TodolistAccessRestrictedException.php
+++ app/app/Exceptions/TodolistAccessRestrictedException.php
@@ -4,7 +4,7 @@ namespace App\Exceptions;
 
 class TodolistAccessRestrictedException extends \Exception
 {
-    public function __construct($message = "todo list access restricted", $code = 400, \Throwable $previous = null)
+    public function __construct($message = "todo list access restricted", $code = 403, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
