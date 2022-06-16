05fafd8331c2617b3a4290c8e13d81c5f5e1bc59
Nikita <m.dudin@dyvotech.io>
2022-06-16 14:09:53 +0300

    make user create command

---
+++ app/app/Console/Commands/CreateUser.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Model\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates user';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $name = $this->ask('What is your name?');
+        $email = $this->ask('What is your email?');
+        $password = $this->secret('What is the password?');
+
+        $user = new User(['name' => $name, 'email' => $email, 'password' => Hash::make($password)]);
+        $user->save();
+        $this->info('User created ' . $user->id);
+        return 0;
+    }
+}
