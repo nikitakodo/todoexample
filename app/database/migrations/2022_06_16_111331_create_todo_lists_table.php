7e6ce1965119130ce12cdbade1d57a54a9ae0d10
Nikita <m.dudin@dyvotech.io>
2022-06-16 20:27:52 +0300

    make todo list table migration

---
+++ app/database/migrations/2022_06_16_111331_create_todo_lists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->boolean('is_completed')->default(false);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->on('users')->references('id')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_lists');
+    }
+}
