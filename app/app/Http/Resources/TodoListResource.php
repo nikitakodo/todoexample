124a2a2edb786df599ea9321b15d1b6d8a21122a
Nikita <m.dudin@dyvotech.io>
2022-06-17 18:27:51 +0300

    fix TodoListResource.php

--- app/app/Http/Resources/TodoListResource.php
+++ app/app/Http/Resources/TodoListResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TodoListResource extends JsonResource
@@ -9,14 +10,15 @@ class TodoListResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
         return [
             'id' => $this->id,
             'title' => $this->title,
+            'is_completed' => $this->is_completed,
             'created_at' => $this->created_at,
         ];
     }
